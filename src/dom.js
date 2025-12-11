0ec299809fc567b7438ede12ba5f8bbc9032c4d5
endri <145276468+EndriGoxhaj@users.noreply.github.com>
2025-12-11 13:20:51 +0100

    Cleaned up code and added direction placement

--- src/dom.js
+++ src/dom.js
@@ -1,10 +1,10 @@
-import { player1 } from "./logic.js";
-
+import { Player } from "./logic.js";
+let player1 = new Player();
+let player2 = new Player();
 const content = document.getElementById("content");
+
 const leftBoard = document.getElementById("leftBoard");
 const rightBoard = document.getElementById("rightBoard");
-
-const ships = player1.navy.ships;
 
 function createBoard(boardId) {
   const board = document.getElementById(boardId);
@@ -32,43 +32,55 @@ const createShip = (size, ship) => {
   }
 };
 
-const shipDivs = (ships = player1.navy.ships) => {
-  let shipContainer = document.createElement("div");
-  shipContainer.id = "shipContainer";
+const shipDivs = (ships) => {
   ships.forEach((ship) => {
     let shipDiv = document.createElement("div");
     shipDiv.classList.add("ship");
     shipDiv.dataset.name = ship.name;
     createShip(ship.size, shipDiv);
-    shipContainer.appendChild(shipDiv);
+    content.appendChild(shipDiv);
   });
-  return shipContainer;
+};
+const boardTrespass = (direction, size, cell) => {
+  if (direction + size > 10) {
+    cell.style.cursor = "not-allowed";
+    return true;
+  }
 };
 
-let selectedShip = null;
-let selectedShipObject = null;
-let hoveredCell = [];
-let direction = "y";
+shipDivs(player1.navy.ships);
+let turn = 1;
 
-(() => {
-  let horizontalButton = document.createElement("button");
-  let verticalButton = document.createElement("button");
-  horizontalButton.textContent = "horizontal";
-  verticalButton.textContent = "vertical";
+const boardPopulate = (board, player) => {
+  let selectedShipDiv = null;
+  let selectedShipObject = null;
+  let hoveredCell = [];
+  let direction = "y";
+  let ships = player.navy.ships;
+  let button = document.createElement("button");
+  button.textContent = `direction = ${direction}`;
+  content.appendChild(button);
 
-  content.append(shipDivs(), horizontalButton, verticalButton);
+  button.addEventListener("click", () => {
+    if (direction === "y") {
+      direction = "x";
+    } else direction = "y";
+
+    button.textContent = `direction = ${direction}`;
+  });
 
   content.querySelectorAll(".ship").forEach((div) => {
     div.addEventListener("click", () => {
-      if (selectedShip) {
-        selectedShip.classList.remove("selected");
+      if (selectedShipDiv) {
+        selectedShipDiv.classList.remove("selected");
       }
       div.classList.add("selected");
-      selectedShip = div;
+      selectedShipDiv = div;
       selectedShipObject = ships.find((ship) => ship.name === div.dataset.name);
     });
   });
-  leftBoard.querySelectorAll(".cell").forEach((cell) => {
+
+  board.querySelectorAll(".cell").forEach((cell) => {
     cell.addEventListener("mouseover", () => {
       if (cell.classList.contains("selected") || !selectedShipObject) {
         return;
@@ -76,28 +88,50 @@ let direction = "y";
       let x = parseInt(cell.dataset.x);
       let y = parseInt(cell.dataset.y);
       let shipLength = selectedShipObject.size;
-      if (direction == "y") {
-        if (y + shipLength > 10) {
-          cell.style.cursor = "not-allowed";
-          return;
-        }
-        for (let i = 0; i < shipLength; i++) {
-          let target = leftBoard.querySelector(
-            `.cell[data-x="${x}"][data-y="${y + i}"]`
-          );
-          target.classList.add("selected");
-          hoveredCell.push(target);
-        }
+
+      if (direction === "y") {
+        if (boardTrespass(y, shipLength, cell)) return;
+      } else {
+        if (boardTrespass(x, shipLength, cell)) return;
+      }
+
+      for (let i = 0; i < shipLength; i++) {
+        let target =
+          direction == "y"
+            ? board.querySelector(`.cell[data-x="${x}"][data-y="${y + i}"]`)
+            : board.querySelector(`.cell[data-x="${x + i}"][data-y="${y}"]`);
+        target.classList.add("selected");
+        hoveredCell.push(target);
       }
     });
     cell.addEventListener("click", () => {
-      if (!selectedShipObject) return;
+      let x = parseInt(cell.dataset.x);
+      let y = parseInt(cell.dataset.y);
+      if (!selectedShipDiv) return;
+      let shipLength = selectedShipObject.size;
+      for (let i = 0; i < hoveredCell.length; i++) {
+        if (hoveredCell[i].classList.contains("shipPlaced")) return;
+      }
+
+      if (direction === "y") {
+        if (boardTrespass(y, shipLength, cell)) return;
+        player.field.placeShipHorizontally(selectedShipObject, x, y);
+      }
+      if (direction === "x") {
+        if (boardTrespass(x, shipLength, cell)) return;
+        player.field.placeShipVertically(selectedShipObject, x, y);
+      }
+
       hoveredCell.forEach((cell) => {
         cell.classList.add("shipPlaced");
-        selectedShipObject = null;
-        let shipContainer = document.getElementById("shipContainer");
       });
-      shipContainer.removeChild(selectedShip);
+      content.removeChild(selectedShipDiv);
+      selectedShipDiv = null;
+      selectedShipObject = null;
+      if (content.querySelectorAll(".ship").length == 0) {
+        content.append(shipDivs(player2.navy.ships));
+        boardPopulate(rightBoard, player2);
+      }
     });
     cell.addEventListener("mouseout", () => {
       hoveredCell.forEach((cell) => {
@@ -107,4 +141,5 @@ let direction = "y";
       hoveredCell.length = 0;
     });
   });
-})();
+};
+boardPopulate(leftBoard, player1);
