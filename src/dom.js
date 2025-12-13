af9775ce3f148a513a3f8440e3fb26220db5d916
endri <145276468+EndriGoxhaj@users.noreply.github.com>
2025-12-13 11:16:34 +0100

    Seperated Dom with render.js again and made the code less coupled

--- src/dom.js
+++ src/dom.js
@@ -1,55 +1,35 @@
 import { Player } from "./logic.js";
+import {
+  createBoard,
+  shipDivs,
+  boardTrespass,
+  renderBoard,
+  returnShip,
+  highlightCells,
+} from "./render.js";
+
 let player1 = new Player();
 let player2 = new Player();
+
+const leftBoard = document.createElement("div");
+leftBoard.classList.add("Board");
+leftBoard.id = "leftBoard";
+
+const rightBoard = document.createElement("div");
+rightBoard.classList.add("Board");
+rightBoard.id = "rightBoard";
+
+const Boards = document.getElementById("Boards");
+Boards.appendChild(leftBoard);
+
 const content = document.getElementById("content");
+content.appendChild(shipDivs(player1.navy.ships));
 
-const leftBoard = document.getElementById("leftBoard");
-const rightBoard = document.getElementById("rightBoard");
+const button = document.createElement("button");
+button.textContent = "direction = y";
 
-function createBoard(boardId) {
-  const board = document.getElementById(boardId);
-
-  for (let x = 0; x < 10; x++) {
-    for (let y = 0; y < 10; y++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      cell.dataset.x = x;
-      cell.dataset.y = y;
-
-      board.appendChild(cell);
-    }
-  }
-}
-
-createBoard("leftBoard");
-createBoard("rightBoard");
-
-const createShip = (size, ship) => {
-  for (let i = 0; i < size; i++) {
-    let shipSize = document.createElement("div");
-    shipSize.classList.add("cell");
-    ship.appendChild(shipSize);
-  }
-};
-
-const shipDivs = (ships) => {
-  ships.forEach((ship) => {
-    let shipDiv = document.createElement("div");
-    shipDiv.classList.add("ship");
-    shipDiv.dataset.name = ship.name;
-    createShip(ship.size, shipDiv);
-    content.appendChild(shipDiv);
-  });
-};
-const boardTrespass = (direction, size, cell) => {
-  if (direction + size > 10) {
-    cell.style.cursor = "not-allowed";
-    return true;
-  }
-};
-
-shipDivs(player1.navy.ships);
-let turn = 1;
+const alert = document.getElementById("alert");
+alert.appendChild(button);
 
 const boardPopulate = (board, player) => {
   let selectedShipDiv = null;
@@ -57,9 +37,6 @@ const boardPopulate = (board, player) => {
   let hoveredCell = [];
   let direction = "y";
   let ships = player.navy.ships;
-  let button = document.createElement("button");
-  button.textContent = `direction = ${direction}`;
-  content.appendChild(button);
 
   button.addEventListener("click", () => {
     if (direction === "y") {
@@ -69,7 +46,9 @@ const boardPopulate = (board, player) => {
     button.textContent = `direction = ${direction}`;
   });
 
-  content.querySelectorAll(".ship").forEach((div) => {
+  createBoard(board);
+
+  alert.querySelectorAll(".ship").forEach((div) => {
     div.addEventListener("click", () => {
       if (selectedShipDiv) {
         selectedShipDiv.classList.remove("selected");
@@ -87,51 +66,15 @@ const boardPopulate = (board, player) => {
       }
       let x = parseInt(cell.dataset.x);
       let y = parseInt(cell.dataset.y);
-      let shipLength = selectedShipObject.size;
 
       if (direction === "y") {
-        if (boardTrespass(y, shipLength, cell)) return;
+        if (boardTrespass(y, shipLength)) return;
       } else {
-        if (boardTrespass(x, shipLength, cell)) return;
+        if (boardTrespass(x, shipLength)) return;
       }
 
-      for (let i = 0; i < shipLength; i++) {
-        let target =
-          direction == "y"
-            ? board.querySelector(`.cell[data-x="${x}"][data-y="${y + i}"]`)
-            : board.querySelector(`.cell[data-x="${x + i}"][data-y="${y}"]`);
-        target.classList.add("selected");
-        hoveredCell.push(target);
-      }
-    });
-    cell.addEventListener("click", () => {
-      let x = parseInt(cell.dataset.x);
-      let y = parseInt(cell.dataset.y);
-      if (!selectedShipDiv) return;
-      let shipLength = selectedShipObject.size;
-      for (let i = 0; i < hoveredCell.length; i++) {
-        if (hoveredCell[i].classList.contains("shipPlaced")) return;
-      }
-
-      if (direction === "y") {
-        if (boardTrespass(y, shipLength, cell)) return;
-        player.field.placeShipHorizontally(selectedShipObject, x, y);
-      }
-      if (direction === "x") {
-        if (boardTrespass(x, shipLength, cell)) return;
-        player.field.placeShipVertically(selectedShipObject, x, y);
-      }
-
-      hoveredCell.forEach((cell) => {
-        cell.classList.add("shipPlaced");
-      });
-      content.removeChild(selectedShipDiv);
-      selectedShipDiv = null;
-      selectedShipObject = null;
-      if (content.querySelectorAll(".ship").length == 0) {
-        content.append(shipDivs(player2.navy.ships));
-        boardPopulate(rightBoard, player2);
-      }
+      hoveredCell = returnShip(board, x, y, direction, selectedShipObject.size);
+      highlightCells(hoveredCell);
     });
     cell.addEventListener("mouseout", () => {
       hoveredCell.forEach((cell) => {
@@ -139,6 +82,39 @@ const boardPopulate = (board, player) => {
         cell.style.cursor = "default";
       });
       hoveredCell.length = 0;
+    });
+    cell.addEventListener("click", () => {
+      let x = parseInt(cell.dataset.x);
+      let y = parseInt(cell.dataset.y);
+      if (!selectedShipDiv) return;
+      for (let i = 0; i < hoveredCell.length; i++) {
+        if (hoveredCell[i].classList.contains("shipPlaced")) return;
+      }
+
+      if (direction === "y") {
+        if (boardTrespass(y, selectedShipObject.size, cell)) return;
+        player.field.placeShipHorizontally(selectedShipObject, x, y);
+      }
+      if (direction === "x") {
+        if (boardTrespass(x, selectedShipObject.size, cell)) return;
+        player.field.placeShipVertically(selectedShipObject, x, y);
+      }
+
+      hoveredCell.forEach((cell) => {
+        cell.classList.add("shipPlaced");
+      });
+      alert.removeChild(selectedShipDiv);
+      selectedShipDiv = null;
+      selectedShipObject = null;
+
+      if (alert.querySelectorAll(".ship").length == 0) {
+        player.shipPlaced = true;
+        if (player === player2 && player.shipPlaced) return;
+        Boards.removeChild(leftBoard);
+        Boards.appendChild(rightBoard);
+        content.appendChild(shipDivs(player2.navy.ships));
+        boardPopulate(rightBoard, player2);
+      }
     });
   });
 };
